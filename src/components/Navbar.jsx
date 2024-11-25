a1352dd5bf6fd172f4da74da8fff3c502aa60f8f
seunosinowo <oluwaseunpaul98@gmail.com>
2024-11-25 09:44:31 +0100

    Nar head

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-col fixed backdrop-blur-md w-full'>
+        <div className='h-20 bg-black flex'>
+            <h1 className='text-white py-5 px-9 font-bold text-[1.5rem]'>Near
+              <span className='text-red-700'>Me</span>
+            </h1>
+        </div>
+
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
